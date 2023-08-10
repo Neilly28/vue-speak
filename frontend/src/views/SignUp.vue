16831f9ae0d40d0310bd77e4cf8f518ce7109bfc
Neilly28 <neilpilarca@gmail.com>
2023-08-10 18:30:40 +0200

    added sign up and login css

--- frontend/src/views/SignUp.vue
+++ frontend/src/views/SignUp.vue
@@ -1,40 +1,62 @@
 <template>
-  <form @submit.prevent="handleSignUp">
-    <h1>Sign Up Here</h1>
-    <div class="flex flex-col items-center justify-center">
-      <div>
-        <label for="username">username</label>
-        <input
-          class="cursor-pointer rounded-full bg-slate-100 px-4 py-2"
-          type="text"
-          id="username"
-          name="username"
-          v-model="username"
-          required
-        />
-      </div>
+  <div class="text-center">
+    <h1 class="mb-4 mt-12 text-4xl font-bold">
+      Do you want to become a teacher on SpeakVue?
+    </h1>
+    <p class="mb-12 text-lg">
+      Complete the teacher application below. Once you are accepted, students
+      will be able to find you!
+    </p>
+  </div>
+  <div>
+    <form
+      @submit.prevent="handleSignUp"
+      class="m-auto flex w-80 flex-col items-center justify-evenly rounded-3xl bg-white pb-6 pt-12 text-center shadow-md"
+    >
+      <div class="flex flex-col justify-center gap-6">
+        <div class="flex flex-col gap-2 text-left">
+          <label for="username">Username</label>
+          <input
+            class="cursor-pointer rounded-full bg-slate-100 px-4 py-2"
+            type="text"
+            id="username"
+            name="username"
+            v-model="username"
+            required
+          />
+        </div>
 
-      <div>
-        <label for="password">password</label>
-        <input
-          class="cursor-pointer rounded-full bg-slate-100 px-4 py-2"
-          type="password"
-          id="password"
-          name="password"
-          v-model="password"
-          required
-        />
+        <div class="flex flex-col gap-2 text-left">
+          <label for="password">Password</label>
+          <input
+            class="cursor-pointer rounded-full bg-slate-100 px-4 py-2"
+            type="password"
+            id="password"
+            name="password"
+            v-model="password"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          :disabled="!username || !password || authStore.isLoading"
+          class="m-auto mt-6 w-24 rounded-3xl border-transparent bg-cyan-200 px-4 py-1 font-semibold text-cyan-800 transition-all hover:bg-cyan-400 disabled:bg-slate-400 disabled:text-slate-100"
+        >
+          Sign Up
+        </button>
+        <div class="flex h-12 flex-col items-center justify-around">
+          <div v-if="error" class="text-sm text-red-500">
+            {{ error }}
+          </div>
+          <router-link
+            class="transition-all ease-in hover:text-cyan-400"
+            to="/login"
+            >Already have an account? Sign in</router-link
+          >
+        </div>
       </div>
-      <button
-        type="submit"
-        :disabled="!username || !password || authStore.isLoading"
-        class="cursor-pointer rounded-full bg-cyan-200 px-4 py-2 font-bold text-cyan-800 transition-all hover:bg-cyan-300 disabled:bg-slate-400 disabled:text-slate-100"
-      >
-        Sign Up
-      </button>
-      <div v-if="error" class="text-2xl text-red-500">{{ error }}</div>
-    </div>
-  </form>
+    </form>
+  </div>
 </template>
 
 <script setup>
